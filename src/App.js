a94233eb3e4a44b3f0a0927e584df1d52d1d4adb
Bruno <bsalasc01@educantabria.es>
2022-04-07 12:01:43 +0200

    borrado de comentarios

--- src/App.js
+++ src/App.js
@@ -23,12 +23,6 @@ function App() {
       {MenuElems.map(function showRoutes(item,index){
         return (<Route path ={item.path}key={uuid()}>{item.componentSite}</Route>);
       })}
-        {/*<Route path = "/" exact ><div/></Route>
-        <Route path = "/Pepe"><MiPrimerComponenteClase nombre = " Pepe " apellido =" Perez " color="red"/></Route>
-        <Route path = "/Juan"><MiPrimerComponenteClase nombre = " Juan " apellido =" Martin "color="blue"/></Route>
-        <Route path = "/Luis"><MiPrimerComponenteClase nombre = " Luis " apellido =" Pueblo "color="green"/></Route>
-        <Route path = "/Manu"><MiPrimerComponenteClase nombre = " Manu " apellido =" Cobos "color="purple"/></Route>    
-        */}
   </Router>
   );
 }
